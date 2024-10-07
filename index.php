aae14953f4cc6581e14b6be744dd877e45e99a5d
Ivan Razmyslov <76975247+Reable@users.noreply.github.com>
2024-10-07 21:57:59 +0300

    ~

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Src\Factory\UserFactory;
 use Src\Service\UserService;
@@ -8,11 +8,6 @@ use Src\Service\UserService;
 $userRepository = UserFactory::createRepository();
 $userService = new UserService($userRepository);
 
-$status = $userService->auth('user1', '12345');
+$status = $userService->auth('user', '12345');
 
 print_r($status);
-
-if ($status === 'OK') {
-    $user = $userService->getUser(23);
-    print_r($user);
-}
